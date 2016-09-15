eeae3bcb10dc9db6e636f053d8fbf04630178071
Jeffrey James <=>
2016-09-15 17:24:19 -0600

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+/*
+
+http://api.openweathermap.org/data/2.5/weather
+?q=London,uk&appid=0bbf7572880155f0d500eca39a583571&units=imperial
+
+*/
+
+// ReactDOM.render(
+//  <Router history={hashHistory}>
+//  	<Route path="/" component={Main}>
+//  		<Route path="countdown" component={Countdown} />
+//  		<IndexRoute component={Timer}/> 
+//  	</Route>
+//  </Router>,
+//   document.getElementById('app')
+// );
+
+ReactDOM.render(
+	<p>Boilerplate3 Project </p>,
+	document.getElementById('app')
+	)
