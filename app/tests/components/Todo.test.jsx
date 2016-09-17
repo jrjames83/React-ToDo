22b21294f4c5fe1ec20ee789353cb7d38e1ccc78
Jeffrey James <=>
2016-09-17 12:12:18 -0600

    added more todo.test.jsx cases

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -11,4 +11,23 @@ describe('Todo', () => {
 	it('should exist', () => {
 		expect(Todo).toExist();
 	})
+
+	it('should call onToggle prop with id on click', () => {
+
+		var todoData = {
+			id: 11,
+			text: "test features", 
+			completed: true
+		};
+
+		var spy = expect.createSpy();
+		var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+
+		var $el = $(ReactDOM.findDOMNode(todo));
+		expect($el).toExist();
+		TestUtils.Simulate.click($el[0]);
+		expect(spy).toHaveBeenCalledWith(11);
+
+
+	})
 })
